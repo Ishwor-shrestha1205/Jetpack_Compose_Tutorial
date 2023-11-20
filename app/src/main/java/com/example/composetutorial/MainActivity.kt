1a9138b5919fe337573f8ee638a39fdfba790fc2
Ishwor-shrestha1205 <shrestha_ishwor@retail-ai.jp>
2023-11-20 12:23:02 +0900

    Preview your function in Android Studio

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,4 +20,11 @@ class MainActivity : ComponentActivity() {
 fun MessageCard(name: String) {
     Text(text = "Hello $name!")
 }
+
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    MessageCard("Android")
+}
+
 
